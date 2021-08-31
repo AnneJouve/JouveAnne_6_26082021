f999ea7dce82519fde187ee7baf4ee1ab6c98e6a
Anne JOUVE <annejouve@iMac-de-Anne.local>
2021-08-31 10:30:12 +0200

    Mise en place des routes sauce : get /api/sauces et /api/sauces/:id, post /api/sauces, put /api/sauces/:id et delete /api/sauces/:id

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,4 +1,4 @@
-const sauce = require('../models/Sauce');
+const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
@@ -32,7 +32,7 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            const filemname = sauce.imageUrl.split('/images/')[1];
+            const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({_id: req.params.id})
                     .then(() => res.status(200).json({message: 'Sauce supprimée' }))
